d19af62f52de3c4d2e781277b1dbc3843e9952aa
sinyuchan <dlabpt02@gmail.com>
2026-06-18 19:40:33 +0900

    fix(post): use named TextStyle import from @tiptap/extension-text-style v3

--- segyo-hub/components/post/RichEditor.tsx
+++ segyo-hub/components/post/RichEditor.tsx
@@ -3,7 +3,7 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
-import TextStyle from '@tiptap/extension-text-style'
+import { TextStyle } from '@tiptap/extension-text-style'
 import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
 import Link from '@tiptap/extension-link'
